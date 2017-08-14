a20b2534586a77fc728ce1cc58a4a3a641b80249
Bhaskar <bhaskar.anil429@gmail.com>
2017-08-14 16:22:30 -0500

    Update 0-24V_ADC_16bit.py

--- 0-24V_ADC_16bit.py
+++ 0-24V_ADC_16bit.py
@@ -6,17 +6,17 @@
 import smbus
 import time
 DL = 0
-DL1 = 0.01
+DL1 = 0.5
 address = 0x68
 # Get I2C bus
 bus = smbus.SMBus(1)
 
 # MCP3428 address, 0x68(104)
 # Send configuration command
-#               0x19(16)Continuous conversion mode, Channel-1, 16-bit Resolution, gain 2
+#               0x18 Continuous conversion mode, Channel-1, 16-bit Resolution, gain 1
 
 while True:
-        bus.write_byte(address, 0x19)
+        bus.write_byte(address, 0x18)
         time.sleep(DL1)
 # MCP3428 address, 0x68(104)
 # Read data back from 0x00(0), 2 bytes
@@ -27,13 +27,13 @@ while True:
         raw_adc = (data[0] * 256) + data[1]
         if raw_adc > 32767 :
                 raw_adc -= 65536
-#        current = (raw_adc * 0.022)
+        voltage = (raw_adc * 0.000616)
 # Output data to screen
-        print "ADC Output channel 1 : %d" %raw_adc
+        print "ADC Voltage Output channel 1 : %.2f" %voltage
         time.sleep(DL)
 
 ####### channel 2
-        bus.write_byte(address, 0x39)
+        bus.write_byte(address, 0x38)
         time.sleep(DL1)
 # MCP3428 address, 0x68(104)
 # Read data back from 0x00(0), 2 bytes
@@ -44,11 +44,11 @@ while True:
         raw_adc = (data[0] * 256) + data[1]
         if raw_adc > 32767 :
                 raw_adc -= 65536
-#        current = (raw_adc * 0.022)
+        voltage = (raw_adc * 0.000614)
 # Output data to screen
-        print "ADC Output channel 2 : %d" %raw_adc
+        print "ADC Voltage Output channel 2 : %.2f" %voltage
 ############ channel 3
-        bus.write_byte(address, 0x59)
+        bus.write_byte(address, 0x58)
         time.sleep(DL1)
 # MCP3428 address, 0x68(104)
 # Read data back from 0x00(0), 2 bytes
@@ -59,12 +59,12 @@ while True:
         raw_adc = (data[0] * 256) + data[1]
         if raw_adc > 32767 :
                 raw_adc -= 65536
- #       current = (raw_adc * 0.022)
+        voltage = (raw_adc * 0.000618)
 # Output data to screen
-        print "ADC Output channel 3 : %d" %raw_adc
+        print "ADC Voltage Output channel 3 : %.2f" %voltage
         time.sleep(DL)
 ####### channel 4
-        bus.write_byte(address, 0x79)
+        bus.write_byte(address, 0x78)
         time.sleep(DL1)
 # MCP3428 address, 0x68(104)
 # Read data back from 0x00(0), 2 bytes
@@ -75,7 +75,7 @@ while True:
         raw_adc = (data[0] * 256) + data[1]
         if raw_adc > 32767 :
                 raw_adc -= 65536
-#        current = (raw_adc * 0.022)
+        voltage = (raw_adc * 0.00061)
 # Output data to screen
-        print "ADC Output channel 4 : %d" %raw_adc
+        print "ADC Voltage Output channel 4 : %.2f" %voltage
         time.sleep(DL)
